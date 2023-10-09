91cd18793a5df721f3008958ea69a75e6b5badb8
Jonathan Taveras <jonathanrtv@outlook.com>
2023-10-09 16:56:44 -0400

    fix: remove shadow

--- src/components/CardAction/CardAction.styles.ts
+++ src/components/CardAction/CardAction.styles.ts
@@ -9,15 +9,15 @@ const styles = createStyleSheet(theme => ({
     borderWidth: 2,
     borderColor: theme.colors.border,
     padding: theme.spacing.sm,
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: -2,
-    },
-    shadowOpacity: 0.34,
-    shadowRadius: 10,
+    // shadowColor: '#000',
+    // shadowOffset: {
+    //   width: 0,
+    //   height: -2,
+    // },
+    // shadowOpacity: 0.34,
+    // shadowRadius: 10,
 
-    elevation: 10,
+    // elevation: 10,
   },
   large: {
     width: scale(BASE_WIDTH),
